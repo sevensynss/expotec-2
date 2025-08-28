3bd61e542de43e9a1859d97ff602482c7609b37f
deadsyns <nicolemssp@gmail.com>
2025-08-28 18:14:55 -0300

    inclusão do modal de login e registro

--- js/script.js
+++ js/script.js
@@ -10,3 +10,38 @@ themeToggle.addEventListener('click', () => {
   body.classList.toggle('light');
   localStorage.setItem('theme', body.classList.contains('light') ? 'light' : 'dark');
 });
+
+/* Função para abrir o modal de login */
+const LoginModal = document.getElementById("loginModal");
+
+function openModal() {
+  LoginModal.style.display = "flex";
+}
+
+function CloseLoginModal() {
+  LoginModal.style.display = "none";
+}
+
+window.addEventListener("click", (event) => {
+  if (event.target === LoginModal) {
+    CloseLoginModal();
+  }
+});
+
+/* Função para abrir o modal de registro */
+
+const SignUpmodal = document.getElementById("SignUpModal");
+
+function SignUpOpenModal() {
+  SignUpmodal.style.display = "flex";
+}
+
+function CloseRegisterModal() {
+  SignUpmodal.style.display = "none";
+}
+
+window.addEventListener("click", (event) => {
+  if (event.target === SignUpmodal) {
+    CloseRegisterModal();
+  }
+});
